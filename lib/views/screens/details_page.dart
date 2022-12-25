ff7a9b14a9f41cdbab9d28e27000789a1679e097
jony166091 <israfilhossain166091@gmail.com>
2022-12-25 22:47:37 +0600

    Update details_page

--- lib/views/screens/details_page.dart
+++ lib/views/screens/details_page.dart
@@ -13,6 +13,8 @@ class DetailScreen extends StatefulWidget {
 }
 
 class _DetailScreenState extends State<DetailScreen> {
+  var step = 0;
+  // var isLogin = false.obs;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,101 +24,175 @@ class _DetailScreenState extends State<DetailScreen> {
       ),
       body: Column(
         children: [
-          Container(
-            child: Center(
-              child: Row(
-                children: [
-                  Container(
-                    padding: EdgeInsets.only(left: 15, bottom: 21),
-                    height: 40,
-                    width: 28,
-                    child: Image.asset(
-                      "assets/images/backarrow.png",
-                      fit: BoxFit.fill,
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(left: 30, right: 30),
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const SizedBox(height: 10),
-                          const Text(
-                            "Easy Travelling",
-                            style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.w600,
-                                fontStyle: FontStyle.italic),
-                          ),
-                          const SizedBox(height: 5),
-                          const SizedBox(
-                            width: 50,
-                          ),
-                          Container(
-                            child: Image.asset(
-                              "assets/images/logo.png",
-                              height: 30,
-                              width: 240,
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 15,
-                          )
-                        ]),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          DefaultTabController(
-              length: 3,
-              child: Padding(
-                padding: EdgeInsets.all(15),
-                child: Row(
+          Header_Section(),
+          Padding(
+            padding: EdgeInsets.all(5),
+            child: Column(
+              children: [
+                Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Container(
-                      width: 90,
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Colors.orange,
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Text(
-                        "Parkeren",
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    Container(
-                      width: 90,
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Colors.orange,
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Text(
-                        "Overnachten",
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    Container(
-                      width: 130,
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 252, 252, 252),
+                    // MaterialButton(
+                    //   color: !isLogin.value ? Colors.white : Colors.amber,
+                    //   onPressed: () {
+                    //     isLogin.value = false;
+                    //   },
+                    //   child: Text('Register'),
+                    // ),
+                    // MaterialButton(
+                    //   color: isLogin.value ? Colors.white : Colors.amber,
+                    //   onPressed: () {
+                    //     isLogin.value = true;
+                    //   },
+                    //   child: Text('Login'),
+                    // ),
+                    InkWell(
+                      onTap: () {
+                        setState(() {
+                          step = 0;
+                        });
+                      },
+                      child: Container(
+                        width: 90,
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(12),
-                          border: Border.all(color: Colors.orange)),
-                      child: Text(
-                        "Boodschappen",
-                        textAlign: TextAlign.center,
+                          border: Border.all(color: Colors.orange),
+                        ),
+                        child: const Text(
+                          "Parkeren",
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        setState(() {
+                          step = 1;
+                        });
+                      },
+                      child: Container(
+                        width: 110,
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          border: Border.all(color: Colors.orange),
+                        ),
+                        child: const Text(
+                          "Overnachten",
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        setState(() {
+                          step = 2;
+                        });
+                      },
+                      child: Container(
+                        width: 130,
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 252, 252, 252),
+                            borderRadius: BorderRadius.circular(12),
+                            border: Border.all(color: Colors.orange)),
+                        child: const Text(
+                          "Boodschappen",
+                          textAlign: TextAlign.center,
+                        ),
                       ),
                     )
                   ],
                 ),
-              ))
+                const SizedBox(
+                  height: 20,
+                ),
+                if (step == 0) Parkeren(),
+                if (step == 1) Overnachten(),
+                if (step == 2) Boodsch(),
+                // (step==1)? Parkeren() : (step==2)?Overnachten() : (step == 3)? Boodsch()
+                // :
+              ],
+            ),
+          ),
         ],
+      ),
+    );
+  }
+
+  Widget Parkeren() {
+    return Column(
+      children: [Text("Hello Parkeren")],
+    );
+  }
+
+  Widget Overnachten() {
+    return Column(
+      children: [Text("Hello Overnachten")],
+    );
+  }
+
+  Widget Boodsch() {
+    return Column(
+      children: [Text("Hello Boodsh")],
+    );
+  }
+}
+
+class Header_Section extends StatelessWidget {
+  const Header_Section({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Row(
+          children: [
+            Container(
+              padding: EdgeInsets.only(left: 15, bottom: 21),
+              height: 40,
+              width: 28,
+              child: Image.asset(
+                "assets/images/backarrow.png",
+                fit: BoxFit.fill,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 30, right: 30),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 10),
+                    const Text(
+                      "Easy Travelling",
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                          fontStyle: FontStyle.italic),
+                    ),
+                    const SizedBox(height: 5),
+                    const SizedBox(
+                      width: 50,
+                    ),
+                    Container(
+                      child: Image.asset(
+                        "assets/images/logo.png",
+                        height: 30,
+                        width: 240,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    )
+                  ]),
+            ),
+          ],
+        ),
       ),
     );
   }
